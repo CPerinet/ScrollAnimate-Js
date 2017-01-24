20b7c2b01a4acbf452b2dc0bbd431b8efe35c7e4
Charles Perinet <charlesperinet@MacBook-Pro-de-Charles.local>
2017-01-24 17:34:47 -0500

    update example, module export bug fixed

--- example/webpack.config.js
+++ example/webpack.config.js
@@ -1,5 +1,3 @@
-var StyleLintPlugin = require('stylelint-webpack-plugin');
-
 const config = {
   entry: {
     app: ['./src/index.js']
@@ -14,26 +12,15 @@ const config = {
     loaders: [
       {
         test: /\.js$/,
-        
-          loaders: ['babel?presets[]=es2015', 'eslint-loader'],  
-        
+        loaders: ['babel?presets[]=es2015'],  
         exclude: /node_modules/
       },
       {
         test: /\.scss$/,
         loaders: ["style", "css", "sass"]
-      },
-      {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'file-loader?mimetype=image/svg+xml'},
-      {test: /\.woff(\?v=\d+\.\d+\.\d+)?$/, loader: "file-loader?mimetype=application/font-woff"},
-      {test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/, loader: "file-loader?mimetype=application/font-woff"},
-      {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "file-loader?mimetype=application/octet-stream"},
-      {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file-loader"}
+      }
     ]
-  },
-
-  plugins: [
-    new StyleLintPlugin()
-  ]
+  }
 }
 
 module.exports = config
